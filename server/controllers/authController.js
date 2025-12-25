d619a13c6e6317df1323f415ab5d70c6fab615a7
Ritesh Singh <yesfirst000@gmail.com>
2025-12-25 11:36:53 +0530

    Backend: Added login routes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,5 @@
 import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 import { sendEmail } from '../utils/sendEmail.js';
 
@@ -78,4 +79,45 @@ export const verifyEmail = async (req, res) => {
     await user.save();
 
     res.status(200).json({ message: "Email verified successfully! You can now login." });
+};
+
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // 1. Find the user
+        const user = await User.findOne({ email: email.toLowerCase() });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        // 2. Check if they verified their OTP
+        if (!user.isVerified) {
+            return res.status(401).json({ message: "Please verify your email first!" });
+        }
+
+        // 3. Compare Password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        // 4. Generate JWT
+        const token = jwt.sign(
+            { id: user._id }, 
+            process.env.JWT_SECRET, 
+            { expiresIn: '7d' } // Stays logged in for 7 days
+        );
+
+        res.status(200).json({
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
